d243268584386c796d73c8e260483025f6bed3a7
Andres Uriza <pa.uriza274@uniandes.edu.co>
2020-02-02 21:15:09 -0500

    hybrid images working

--- code/__init__.py
+++ code/__init__.py
@@ -1,1 +1,2 @@
+from .hybrid import *
 from .blended import *
--- main.py
+++ main.py
@@ -5,8 +5,18 @@ import matplotlib.pyplot as plt
 from code import *
 
 cwd = path.abspath('')
-mafe = img_as_float(imread(path.join(cwd,'img','mafe_bebe.jpeg')))
+mafe = img_as_float(imread(path.join(cwd,'img','mafe_bebe.jpg')))
 andres = img_as_float(imread(path.join(cwd,'img','andres_bebe.jpg')))
+plt.imshow(andres)
+plt.axis('off')
+plt.show()
+
+
+hybrid_im = hybrid_image(mafe,andres,4)
+plt.imshow(hybrid_im)
+plt.axis('off')
+plt.show()
+
 blended_im = blended_image(mafe,andres,9,1)
 plt.imshow(blended_im)
 plt.axis('off')
